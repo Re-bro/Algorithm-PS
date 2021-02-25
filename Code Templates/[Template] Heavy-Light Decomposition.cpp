d0b87866c3bc5066a339b1108eaedd034938e436
Rebro <pjh6792@gmail.com>
2021-02-25 10:36:59 +0900

    21.02.25

--- Code Templates/[Template] Heavy-Light Decomposition.cpp
+++ Code Templates/[Template] Heavy-Light Decomposition.cpp
@@ -17,19 +17,21 @@ using namespace std;
 int sz[MAXV], dep[MAXV], par[MAXV], top[MAXV], in[MAXV], out[MAXV];
 vector<int> adj[MAXV];
 int num;
-void dfs1(int u) { //make tree
+void dfs1(int u, int p) { //make tree
 	sz[u] = 1;
 	for (auto& v : adj[u]) {
+		if (v == p) continue;
 		dep[v] = dep[u] + 1; par[v] = u;
-		dfs1(v); sz[u] += sz[v];
+		dfs1(v, u); sz[u] += sz[v];
 		if (sz[v] > sz[adj[u][0]]) swap(v, adj[u][0]);
 	}
 }
-void dfs2(int u) { //make hld
+void dfs2(int u, int p) { //make hld
 	in[u] = ++num;
 	for (int v : adj[u]) {
+		if (v == p) continue;
 		top[v] = (v == adj[u][0] ? top[u] : v);
-		dfs2(v);
+		dfs2(v, u);
 	}
 	out[u] = num;
 }
@@ -38,15 +40,12 @@ int query(int a, int b) {
 	int ret = 0;
 	while (top[a] != top[b]) {
 		if (dep[top[a]] < dep[top[b]]) swap(a, b);
-		int st = top[a];
 		ret += seg.query(in[top[a]], in[a]); //segtree range query [in[top[a]], in[a]]
 		a = par[top[a]];
 	}
 	if (dep[a] > dep[b]) swap(a, b);
 	ret += seg.query(in[a], in[b]); //segtree range query [in[a], in[b]]
-	return ret;
-	if (dep[a] > dep[b]) swap(a, b);
-	
+	return ret;	
 }
 int main(void) {
 	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
