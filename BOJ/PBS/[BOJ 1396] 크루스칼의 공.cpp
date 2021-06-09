dbaf38479a287226eadcacb4ec12bb0d1c2fcfd6
Rebro <pjh6792@gmail.com>
2021-06-09 22:52:34 +0900

    21.06.09

--- BOJ/PBS/[BOJ 1396] 크루스칼의 공.cpp
+++ BOJ/PBS/[BOJ 1396] 크루스칼의 공.cpp
@@ -1,22 +1,17 @@
 /*
 	[BOJ 1396] 크루스칼의 공
 	Algorithm : Union-Find / Parallel Binary Search
+	//O(logm(m+Q)) : while문 횟수 = logm, 간선 한번 스위핑 = m, 쿼리들 갱신하는 횟수 = Q
 */
 
 #include<bits/stdc++.h>
 #define all(v) v.begin(), v.end()
-#define sz(x) (int)x.size()
 #define ini(x, y) memset(x, y, sizeof(x));
 #define pb push_back
 #define fi first
 #define se second
 using namespace std;
-using ll = long long;
 using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-const int MOD = (int)1e9 + 7;
-const int MAX = (int)2e9;
-const double PI = acos(-1);
 
 int P[101010], l[101010], r[101010];
 pii ans[101010];
@@ -48,17 +43,13 @@ int main(void) {
 	for (int i = 0; i < q; i++) {
 		cin >> query[i].fi >> query[i].se;
 	}
-	for (int i = 0; i < q; i++) l[i] = 0, r[i] = m;
-	//O(logm(m+Q))
-	//while문 횟수 = logm
-	//간선 한번 스위핑 = m
-	//쿼리들 갱신하는 횟수 = Q
+	for (int i = 0; i < q; i++) l[i] = 1, r[i] = m;
 	while (1) {
-		for (int i = 0; i <= m; i++) g[i].clear();
+		for (int i = 1; i <= m; i++) g[i].clear();
 		bool chk = false;
 		// O(Qlogm)
 		for (int i = 0; i < q; i++) {
-			if (l[i] != r[i]) {
+			if (l[i] <= r[i]) {
 				chk = true;
 				g[(l[i] + r[i]) / 2].pb(i);
 			}
@@ -66,20 +57,20 @@ int main(void) {
 		if (!chk) break;
 		ini(P, -1);
 		//O(m + Q)
-		for (int i = 0; i < m; i++) {
-			merge(edge[i].se.fi, edge[i].se.se);
+		for (int i = 1; i <= m; i++) {
+			merge(edge[i - 1].se.fi, edge[i - 1].se.se);
 			for (int j : g[i]) {
 				if (find(query[j].fi) == find(query[j].se)) {
-					ans[j].fi = edge[i].fi;
+					ans[j].fi = edge[i - 1].fi;
 					ans[j].se = -P[find(query[j].fi)];
-					r[j] = i;
+					r[j] = i - 1;
 				}
 				else l[j] = i + 1;
 			}
 		}
 	}
 	for (int i = 0; i < q; i++) {
-		if (l[i] == m) cout << -1 << '\n';
+		if (l[i] > m) cout << -1 << '\n';
 		else cout << ans[i].fi << ' ' << ans[i].se << '\n';
 	}
 }
