62a28a70eec8a162398f8eba3d6e061a7f7afc74
Rebro <pjh6792@gmail.com>
2022-03-07 21:40:27 +0900

    22.03.07

---
+++ USACO/USACO 2021 December Contest/[BOJ 23878] Lonely Photo.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int n; cin >> n;
+	string s; cin >> s;
+	ll ans = 0;
+	for (int i = 0; i < n; i++) {
+		ll l = 0, r = 0;
+		if (i > 0 && s[i] != s[i - 1]) {
+			l++;
+			for (int j = i - 2; j >= 0; j--) {
+				if (s[j] == s[i]) break;
+				l++;
+			}
+		}
+		if (i < n - 1 && s[i] != s[i + 1]) {
+			r++;
+			for (int j = i + 2; j < n; j++) {
+				if (s[j] == s[i]) break;
+				r++;
+			}
+		}
+		ans += l * r + max(l - 1, 0LL) + max(r - 1, 0LL);
+	}
+	cout << ans;
+}
